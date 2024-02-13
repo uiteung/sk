247dd6c14141f049bcf047344fdcc6c3a1b94fba
Bachtiar21 <bachtiarramadhan26@gmail.com>
2024-02-13 09:52:34 +0700

    create file js untuk persiapan post

--- static/js/controller/cihuyred.js
+++
@@ -1,7 +0,0 @@
-import {
-    CihuyTokRed,
-  } from "https://c-craftjs.github.io/link/link.js";
-  
-  const redirecturl = "https://euis.ulbi.ac.id";
-  const token = "login";
-  CihuyTokRed(token, redirecturl);
--- static/js/controller/template.js
+++ static/js/controller/template.js
@@ -1,16 +1,2 @@
-// Endpoint Get All
-export let UrlGetAllMasukHarian = "https://hris_backend.ulbi.ac.id/presensi/datapresensi/masukharian";
-export let UrlGetAllPulangHarian = "https://hris_backend.ulbi.ac.id/presensi/datapresensi/pulangharian";
-export let UrlGetAllPresensi = "https://hris_backend.ulbi.ac.id/presensi/datapresensi";
-export let UrlGetAllPresensiPulang = "https://hris_backend.ulbi.ac.id/presensi/datapresensi/pulang";
-export let UrlGetAllCommitsRekapHarian = "https://hris_backend.ulbi.ac.id/api/v2/commits/rekapharian";
-export let UrlGetAllPerforma = "https://hris_backend.ulbi.ac.id/api/v2/commits/performance";
-
-// Endpoint Get By Id
-export let UrlGetKaryawanById = "https://hris_backend.ulbi.ac.id/presensi/datapresensi/getkaryawan";
-
-// Endpoint Post
-export let UrlPostDataKaryawan = "https://hris_backend.ulbi.ac.id/presensi/datakaryawan/postdata";
-
-// Endpoint Update
-export let UrlPutDataKaryawan = "https://hris_backend.ulbi.ac.id/presensi/datakaryawan/updatedata/";
+// Endpoint POST
+export let UrlPostSK = "";
