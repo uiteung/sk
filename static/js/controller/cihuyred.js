1e2ee4581ae1a86ae7f646f942fab181aa0c10c8
Bachtiar21 <bachtiarramadhan26@gmail.com>
2024-02-13 09:45:23 +0700

    first commit

---
+++ static/js/controller/cihuyred.js
@@ -0,0 +1,7 @@
+import {
+    CihuyTokRed,
+  } from "https://c-craftjs.github.io/link/link.js";
+  
+  const redirecturl = "https://euis.ulbi.ac.id";
+  const token = "login";
+  CihuyTokRed(token, redirecturl);
