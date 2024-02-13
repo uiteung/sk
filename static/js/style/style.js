c3bfb993bbef4394c26154da807334ebd384414d
Bachtiar21 <bachtiarramadhan26@gmail.com>
2024-02-13 11:26:29 +0700

    create style change form input

--- static/js/style/style.js
+++ static/js/style/style.js
@@ -7,3 +7,18 @@ window.addEventListener("load", () => {
         document.body.removeChild("loader");
     })
 })
+
+// Untuk Ganti-Ganti Form
+document.getElementById('statusSelect').addEventListener('change', function() {
+    var selectedOption = this.value;
+    if (selectedOption === '2324-1') {
+        document.getElementById('2324-1').removeAttribute('hidden');
+        document.getElementById('2324-2').setAttribute('hidden', 'true');
+    } else if (selectedOption === '2324-2') {
+        document.getElementById('2324-2').removeAttribute('hidden');
+        document.getElementById('2324-1').setAttribute('hidden', 'true');
+    } else {
+        document.getElementById('2324-1').setAttribute('hidden', 'true');
+        document.getElementById('2324-2').setAttribute('hidden', 'true');
+    }
+});
