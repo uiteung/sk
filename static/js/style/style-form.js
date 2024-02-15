7637a60c5a806219cc754693893ad34b37270ff8
Bachtiar21 <bachtiarramadhan26@gmail.com>
2024-02-15 12:22:05 +0700

    style js for page

---
+++ static/js/style/style-form.js
@@ -0,0 +1,14 @@
+// Untuk Ganti-Ganti Form
+document.getElementById('thnAjaran').addEventListener('change', function() {
+    var selectedOption = this.value;
+    if (selectedOption === '2324-1') {
+        document.getElementById('2324-1').removeAttribute('hidden');
+        document.getElementById('2324-2').setAttribute('hidden', 'true');
+    } else if (selectedOption === '2324-2') {
+        document.getElementById('2324-2').removeAttribute('hidden');
+        document.getElementById('2324-1').setAttribute('hidden', 'true');
+    } else {
+        document.getElementById('2324-1').setAttribute('hidden', 'true');
+        document.getElementById('2324-2').setAttribute('hidden', 'true');
+    }
+});
