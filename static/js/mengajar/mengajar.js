6c2d3c49aa8b93b8d6ecd0b217b3a16836ff73c8
Bachtiar21 <bachtiarramadhan26@gmail.com>
2024-02-15 12:42:00 +0700

    create function post untuk sk mengajar

--- static/js/mengajar/mengajar.js
+++ static/js/mengajar/mengajar.js
@@ -1,86 +1,55 @@
 // Import library yang dibutuhkan
 import { postFileWithHeader } from "https://cdn.jsdelivr.net/gh/jscroot/api@0.0.2/croot.js";
-import { UrlPostSK, UrlPostSKByFolder } from "./controller/template.js";
-// import { getValue } from "https://cdn.jsdelivr.net/gh/jscroot/element@0.0.5/croot.js";
-import { token } from "./controller/cookies.js";
+import { UrlPostSKByFolder } from "../controller/template.js";
+import { token } from "../controller/cookies.js";
 
-const thnAjaran = document.getElementById('thnAjaran');
-thnAjaran.addEventListener('change', () => {
-    const selectedYear = thnAjaran.value;
-    console.log(selectedYear); // Untuk memastikan nilai tahun ajaran yang dipilih
+// Ambil elemen-elemen dari HTML
+const thnAjaran = document.getElementById('thnAjaranBimbing');
+const submitButton = document.getElementById('submitButton');
 
-    // Update URL untuk pengiriman SK berdasarkan tahun ajaran yang dipilih
-    const PostSKByFolder = UrlPostSKByFolder + `${selectedYear}/mengajar`;
+// Tambahkan event listener pada tombol submit
+submitButton.addEventListener('click', () => {
+    // Ambil nilai yang dipilih saat tombol diklik
+    const selectedThnAjaran = thnAjaran.value;
 
-    // Untuk Post File SK 2023 2024 Ganjil
-    const submitButton1 = document.getElementById('submitButton1');
-    submitButton1.addEventListener('click', () => {
-        confirmUpload1();
-    });
+    // Update URL untuk pengiriman SK berdasarkan tahun ajaran dan pelaksanaan pendidikan yang dipilih
+    const postSKByFolder = UrlPostSKByFolder + `${selectedThnAjaran}/mengajar`;
 
-    function confirmUpload1() {
-        Swal.fire({
-            title: 'Konfirmasi',
-            text: 'Anda yakin ingin mengunggah file SK?',
-            icon: 'warning',
-            showCancelButton: true,
-            confirmButtonColor: '#3085d6',
-            cancelButtonColor: '#d33',
-            confirmButtonText: 'Ya, Unggah!',
-            cancelButtonText: 'Batal'
-        }).then((result) => {
-            if (result.isConfirmed) {
-                postFileWithHeader(PostSKByFolder, "login", token, "skUpload2324Ganjil", "file", responsePostSK1);
-            }
-        });
-    };
-
-    function responsePostSK1(result) {
-        Swal.fire({
-            icon : 'success',
-            title : 'Sukses!',
-            text : 'File SK berhasil diunggah.',
-            showConfirmButton : false,
-            timer : 1500
-        }).then(() => {
-            window.location.replace("index.html");
-        });
-        console.log(result);
-    };
-
-    // Untuk Post File SK 2023 2024 Ganjil
-    const submitButton2 = document.getElementById('submitButton2');
-    submitButton2.addEventListener('click', () => {
-        confirmUpload2();
-    });
-
-    function confirmUpload2() {
-        Swal.fire({
-            title: 'Konfirmasi',
-            text: 'Anda yakin ingin mengunggah file SK?',
-            icon: 'warning',
-            showCancelButton: true,
-            confirmButtonColor: '#3085d6',
-            cancelButtonColor: '#d33',
-            confirmButtonText: 'Ya, Unggah!',
-            cancelButtonText: 'Batal'
-        }).then((result) => {
-            if (result.isConfirmed) {
-                postFileWithHeader(PostSKByFolder, "login", token, "skUpload2324Ganjil", "file", responsePostSK2);
-            }
-        });
-    }
-
-    function responsePostSK2(result) {
-        Swal.fire({
-            icon : 'success',
-            title : 'Sukses!',
-            text : 'File SK berhasil diunggah.',
-            showConfirmButton : false,
-            timer : 1500
-        }).then(() => {
-            window.location.replace("index.html");
-        });
-        console.log(result);
-    };
+    // Tampilkan konfirmasi dan kirim file jika dikonfirmasi
+    confirmUpload(postSKByFolder);
 });
+
+// Fungsi untuk menampilkan konfirmasi upload
+function confirmUpload(postSKByFolder) {
+    Swal.fire({
+        title: 'Konfirmasi',
+        text: 'Anda yakin ingin mengunggah file SK?',
+        icon: 'warning',
+        showCancelButton: true,
+        confirmButtonColor: '#3085d6',
+        cancelButtonColor: '#d33',
+        confirmButtonText: 'Ya, Unggah!',
+        cancelButtonText: 'Batal'
+    }).then((result) => {
+        if (result.isConfirmed) {
+            // Kirim file jika dikonfirmasi
+            postFileWithHeader(postSKByFolder, "login", token, "skUpload", "file", responsePostSK);
+        }
+    });
+}
+
+// Fungsi untuk menangani respons dari pengiriman file
+function responsePostSK(result) {
+    Swal.fire({
+        icon: 'success',
+        title: 'Sukses!',
+        text: 'File SK berhasil diunggah.',
+        showConfirmButton: false,
+        timer: 1500
+    }).then(() => {
+        window.location.replace("form-upload-membimbing.html");
+    });
+
+    // Log respons dari pengiriman file
+    console.log(result);
+}
